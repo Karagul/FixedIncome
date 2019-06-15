933332dac9aa1b8fc26338ad3a5c8ae21a803b0a
Taylor <danie665@umn.edu>
2019-06-15 17:31:24 +0000

    Learning python regression

--- learn_regression.py
+++ learn_regression.py
@@ -1,6 +1,7 @@
 import numpy
 from sklearn.linear_model import LinearRegression
 
-x = np.array([5, 15, 25, 35, 45, 55]).reshape(-1, 1))
-y = np.array([5, 20, 14, 32, 22, 38])
+x = numpy.array([5, 15, 25, 35, 45, 55]).reshape(-1, 1)
+y = numpy.array([5, 20, 14, 32, 22, 38])
 
+model = LinearRegression().fit(x, y)
