54903100b07d07a25a4b45e9df29cdcb7a6c7336
Ubuntu <ubuntu@ip-172-26-11-30.us-west-2.compute.internal>
2019-06-15 17:05:17 +0000

    Figuring out sklearn regression

--- learn_regression.py
+++ learn_regression.py
@@ -1,3 +1,6 @@
-from sklearn.linear_model import linear_model
+import numpy
+from sklearn.linear_model import LinearRegression
 
-reg = linear_model.LinearRegression()
+x = np.array([5, 15, 25, 35, 45, 55]).reshape(-1, 1))
+y = np.array([5, 20, 14, 32, 22, 38])
+
