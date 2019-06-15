4df05c0ce59671b1defdb653e7bcce0246ad19fd
Taylor <danie665@umn.edu>
2019-06-15 17:51:11 +0000

    Creating template multiple regression model

--- learn_regression.py
+++ learn_regression.py
@@ -1,7 +1,23 @@
 import numpy
 from sklearn.linear_model import LinearRegression
 
-x = numpy.array([5, 15, 25, 35, 45, 55]).reshape(-1, 1)
-y = numpy.array([5, 20, 14, 32, 22, 38])
+# create data
+x = [[0,1],
+    [5, 1],
+    [15, 2],
+    [25, 5],
+    [35, 11],
+    [45, 15],
+    [55, 34],
+    [60, 35]]
+y = [4, 5, 20, 14, 32, 22, 38, 43]
+x, y = numpy.array(x), numpy.array(y)
 
+# create model
 model = LinearRegression().fit(x, y)
+
+# retrieve score
+r_sq = model.score(x, y)
+print('coefficient of determination:', r_sq)
+print('intercept', model.intercept_)
+print('slope', model.coef_)
